d3ff41a3b137a61aae421e087c0f8abc5a2c7060
ryanwardle <wardle2106@gmail.com>
2019-12-27 16:14:53 -0500

    continuing work

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members.js')
+const members = require('./Members.js');
+const logger = require('./middleware/logger.js')
 const app = express();
 
 
+// Init middleware
+app.use(logger);
 
 // gets all members
 app.get('/api/members', (req, res) => {
   res.json(members);
+});
+
+// get single members
+app.get('/api/members/:id', (req, res) => {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)))
 });
 
 // set static folder
